ff266f4ca39f4da2ba040935210467caa9143f30
Vipulgarg192 <vipulgarg192gmail.com>
2019-06-18 21:03:25 -0400

    users group created

---
+++ HappyLifeSwift2.0/users/Buyer.swift
@@ -0,0 +1,18 @@
+//
+//  Buyer.swift
+//  HappyLifeSwift2.0
+//
+//  Created by MacStudent on 2019-06-18.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+class Buyer: Users {
+    
+    var accountEmail : String
+    
+    override init() {
+        self.accountEmail = String()
+    }
+}
